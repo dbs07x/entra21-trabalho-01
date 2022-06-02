f5e78530e1d7428c0012e364aa740898e922c46f
Gustavo Lemos <gustavo.aulas@outlook.com>
2022-06-01 22:01:56 -0300

    Início questão02.

--- TrabalhoOrientacaoObjetos01/Questao02/ExecutarCalendario.cs
+++ TrabalhoOrientacaoObjetos01/Questao02/ExecutarCalendario.cs
@@ -4,14 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Objetivo: Dado a entrada de uma data deverá escrever a mesma por extenso.
+//● Classe: Calendário
+//○ Propriedades:
+//■ Data: DateTime
+//○ Métodos:
+//■ ObterMesPorExtenso
+//■ ObterDiaPorExtenso
+//■ ObterAnoPorExtenso
+//■ ObterDataCompletaPorExtenso
+
+//● Cenários de teste unitários:
+//○ Cenário 01: Validar todos os meses por extenso;
+//○ Cenário 02: Validar todos os dias por extenso;
+//○ Cenário 03: Validar todos os anos a partir de 1970 até 2030 por extenso;
+//○ Cenário 04: validar doze datas, sendo uma data por mês.
+//● Classe principal: ExecutarCalendario
+
+//○ Conter método executar, instanciando objeto da classe citada acima,
+//permitindo o usuário escolher qual dos métodos citados acima ele deseja
+//executar.
+
 namespace TrabalhoOrientacaoObjetos01.Questao02
 {
     public class ExecutarCalendario
     {
         public void Executar()
         {
+            Calendario executarCalendario = new Calendario();
+            Console.Write("Digite a data: ");
+            executarCalendario.Data = Convert.ToDateTime(Console.ReadLine());
 
+            Console.Write($"A data é: {executarCalendario.Data.ToString("dd/MM/yyyy")}");
         }
-
+        
     }
 }
